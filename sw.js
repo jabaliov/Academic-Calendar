a3936184d77721470142ed3c9d6aef4c9d23294e
Mohammed Jabali <mr.makkawi@gmail.com>
2026-01-15 18:09:26 +0300

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,5 +1,17 @@
-const CACHE_NAME = 'uni-planner-v1';
-const assets = ['./', './index.html', './style.css', './script.js'];
+const CACHE_NAME = 'uni-planner-v2';
+// تحديث القائمة لتشمل الملفات الجديدة المقسمة
+const assets = [
+    './',
+    './index.html',
+    './style.css',
+    './config.js',
+    './storage.js',
+    './utils.js',
+    './ui-manager.js',
+    './calendar-engine.js',
+    './app.js',
+    './manifest.json'
+];
 
 self.addEventListener('install', e => {
     e.waitUntil(caches.open(CACHE_NAME).then(cache => cache.addAll(assets)));
