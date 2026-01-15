f800a3e78a8033b0f89f98937090321aec1a6acf
Mohammed Jabali <mr.makkawi@gmail.com>
2026-01-15 17:19:43 +0300

    Update utils.js

--- utils.js
+++ utils.js
@@ -4,7 +4,8 @@ const Utils = {
     getHijriDate: (date) => {
         try {
             return new Intl.DateTimeFormat('ar-SA-u-ca-islamic-uma-nu-latn', {
-                day: 'numeric', month: 'long'
+                day: 'numeric',
+                month: 'long'
             }).format(date);
         } catch (e) { return ''; }
     },
@@ -13,7 +14,11 @@ const Utils = {
         const blob = new Blob([JSON.stringify(data, null, 2)], { type: 'application/json' });
         const url = URL.createObjectURL(blob);
         const a = document.createElement('a');
-        a.href = url; a.download = filename; a.click();
+        a.href = url;
+        a.download = filename;
+        a.click();
         URL.revokeObjectURL(url);
-    }
+    },
+
+    generateId: (prefix = 'id') => `${prefix}_${Date.now()}_${Math.random().toString(36).substr(2, 5)}`
 };
