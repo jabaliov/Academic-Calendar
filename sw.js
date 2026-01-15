7bb655a594d36267f309bd2ee7970a9c66424afb
Mohammed Jabali <mr.makkawi@gmail.com>
2026-01-15 20:16:01 +0300

    Update sw.js

--- sw.js
+++ sw.js
@@ -31,3 +31,9 @@ self.addEventListener('activate', e => {
 self.addEventListener('fetch', e => {
     e.respondWith(caches.match(e.request).then(res => res || fetch(e.request)));
 });
+
+self.addEventListener('message', (event) => {
+    if (event.data && event.data.type === 'SKIP_WAITING') {
+        self.skipWaiting();
+    }
+});
