96329567591b372f926ddb0fac5e214c85327466
Mohammed Jabali <mr.makkawi@gmail.com>
2026-01-15 17:16:13 +0300

    Create utils.js

---
+++ utils.js
@@ -0,0 +1,19 @@
+const Utils = {
+    formatDate: (date) => date.toISOString().split('T')[0],
+
+    getHijriDate: (date) => {
+        try {
+            return new Intl.DateTimeFormat('ar-SA-u-ca-islamic-uma-nu-latn', {
+                day: 'numeric', month: 'long'
+            }).format(date);
+        } catch (e) { return ''; }
+    },
+
+    downloadJSON: (data, filename) => {
+        const blob = new Blob([JSON.stringify(data, null, 2)], { type: 'application/json' });
+        const url = URL.createObjectURL(blob);
+        const a = document.createElement('a');
+        a.href = url; a.download = filename; a.click();
+        URL.revokeObjectURL(url);
+    }
+};
