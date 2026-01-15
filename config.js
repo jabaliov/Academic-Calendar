e81bbb55763832bcc326e4402a0239304d6a8933
Mohammed Jabali <mr.makkawi@gmail.com>
2026-01-15 17:15:16 +0300

    Create config.js

---
+++ config.js
@@ -0,0 +1,15 @@
+const DEFAULT_DATA = {
+    config: { startDate: '', endDate: '' },
+    courses: [],
+    holidays: [],
+    periods: [],
+    procedures: [],
+    events: []
+};
+
+// ألوان الحالات
+const STATUS_COLORS = {
+    holidays: 'text-orange-600',
+    periods: 'text-purple-600',
+    procedures: 'text-emerald-600'
+};
